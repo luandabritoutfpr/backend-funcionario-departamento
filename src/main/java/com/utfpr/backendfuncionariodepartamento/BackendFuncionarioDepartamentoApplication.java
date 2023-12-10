6639ae7fad68fb747f8b91cd83d71cff77e04fbe
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2023-12-10 17:50:42 -0300

    projeto inicial

---
+++ src/main/java/com/utfpr/backendfuncionariodepartamento/BackendFuncionarioDepartamentoApplication.java
@@ -0,0 +1,13 @@
+package com.utfpr.backendfuncionariodepartamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFuncionarioDepartamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFuncionarioDepartamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utfpr/backendfuncionariodepartamento/BackendFuncionarioDepartamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.backendfuncionariodepartamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFuncionarioDepartamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
